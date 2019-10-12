19e3860534a36dfd3f5733fc8fb1d6a1f9acc123
Amir Massarwa <amassarwi@gmail.com>
2019-10-12 23:32:47 +0200

    basic burger builder & module styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
+
+import styles from './Layout.module.css';
 
 const layout = ({ children }) => (
   <Aux>
     <div>Toolbar, Siderdrawer, backdrop</div>
-    <main>
+    <main className={styles.Content}>
       {children}
     </main>
   </Aux>
