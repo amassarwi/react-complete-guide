ec98967e3724abfe8a0dbdc0fe122f80f6c9228b
Amir Massarwa <amassarwi@gmail.com>
2019-10-13 19:55:28 +0200

    style label in build control

--- src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.jsx
@@ -4,7 +4,7 @@ import styles from './BuildControl.module.css';
 
 const buildControl = ({label, added, removed, disabled}) => (
   <div className={styles.BuildControl}>
-    <div>{label}</div>
+    <div className={styles.Label}>{label}</div>
     <button className={styles.Less} onClick={removed} disabled={disabled}>Less</button>
     <button className={styles.More} onClick={added}>More</button>
   </div>
