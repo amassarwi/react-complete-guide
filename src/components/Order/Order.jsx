fab6fe46c74df510fbd6bfb2d6b0e6a6129e83b4
Amir Massarwa <amassarwi@gmail.com>
2019-10-20 00:47:16 +0200

    output fetched orders data

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './Order.module.css';
 
-const order = () => (
-  <div className={styles.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>USD 5.45</strong></p>
-  </div>
-);
+const order = ({ingredients, price}) => {
+  const ingredientsList = [];
+  for (let ingredientName in ingredients) {
+    ingredientsList.push({
+      amount: ingredients[ingredientName],
+      name: ingredientName,
+    })
+  }
+
+  let ingredientsOutput = ingredientsList.map(ig => {
+    return <span 
+      style={{textTransform: 'capitalize', display: 'inline-block', margin: '0 8px', border: '1px solid #ccc', padding: '5px'}}
+      key={ig.name}>{ig.name} ({ig.amount})</span>
+  })
+
+  return (
+    <div className={styles.Order}>
+      <p>Ingredients: {ingredientsOutput}</p>
+      <p>Price: <strong>USD {price}</strong></p>
+    </div>
+  );
+}
 
 export default order;
