45d5556a3278737506ac5197c82623f1bf7c60e8
Amir Massarwa <amassarwi@gmail.com>
2019-11-10 00:40:50 +0100

    post signup to firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as at from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,16 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyCk9bMuW0Eka8Cw6_MCEM2jYpxhKHrmPAU`, {
+      email,
+      password,
+      returnSecureToken: true,
+    })
+      .then((res) => {
+        dispatch(authSuccess(res.data));
+      })
+      .catch((error) => {
+        dispatch(authFail(error));
+      });
   }
 }
