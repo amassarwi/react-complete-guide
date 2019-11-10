e6751c31c3d259ce2f3ec883ef6a3b52ed591194
Amir Massarwa <amassarwi@gmail.com>
2019-11-10 19:26:43 +0100

    tiny fixes

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -14,7 +14,7 @@ const sideDrawer = ({closed, open, isAuth}) => {
   return (
     <Aux>
       <Backdrop show={open} clicked={closed} />
-      <div className={attachedClasses.join(' ')}>
+      <div className={attachedClasses.join(' ')} onClick={closed}>
         <div className={styles.Logo}>
           <Logo />
         </div>
